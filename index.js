d66072256b313f038c03cf023a9c6729028aad1c
jourdanmauricio <jourdanmauricio@gmail.com>
2023-11-30 17:46:04 -0300

    Add Cors

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
+// const whitelist = // son los mismos (si pego los links platzi me impide comentar)
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'), false);
+//     }
+//   }
+// }
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en Express');
